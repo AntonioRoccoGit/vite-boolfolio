cd89dd9f77377694e4ccd08dfc2002672c7ec005
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-06-13 16:41:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+import {store} from './store';
+export default{
+  data() {
+    return {
+      store,
+      projects: []
+    }
+  },
+  mounted(){
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios
+      .get(`${this.store.apiUrl}${this.store.apiProjects}`)
+      .then(resp=>{
+        this.projects = resp.data.results;
+        console.log(this.projects);
+      });
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="text-center">Backend-Frontend</h2>
+
+  </div>
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+</style>
