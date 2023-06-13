33bfe685922067d5e9696ebe646ec2469b437c1f
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-06-13 17:18:44 +0200

    added: projects card in appvue

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script>
 import axios from 'axios';
-import {store} from './store';
-export default{
+import { store } from './store';
+export default {
   data() {
     return {
       store,
       projects: []
     }
   },
-  mounted(){
+  mounted() {
     this.getProjects();
   },
   methods: {
     getProjects() {
       axios
-      .get(`${this.store.apiUrl}${this.store.apiProjects}`)
-      .then(resp=>{
-        this.projects = resp.data.results;
-        console.log(this.projects);
-      });
+        .get(`${this.store.apiUrl}${this.store.apiProjects}`)
+        .then(resp => {
+          this.projects = resp.data.results;
+          console.log(this.projects);
+        });
     }
   }
 }
@@ -26,8 +26,18 @@ export default{
 
 <template>
   <div class="container">
-    <h2 class="text-center">Backend-Frontend</h2>
-
+    <h2 class="text-center p-4">Backend-Frontend</h2>
+    <div class="row row-cols-4 g-4">
+      <div v-for="(project, index ) in projects" class="col">
+        <div class="card h-100" :key="index">
+          <img :src="`${this.store.apiUrl}${this.store.imgUrlPrefix}${project.thumb}`" class="card-img-top" :alt="project.slug">
+          <div class="card-body h-100 d-flex flex-column justify-content-end">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
